0d9376f48b8a57f3de9b8a7b820fee6972e74185
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-07-28 16:55:49 +0400

    solved mkos

--- FibonacciIterator/FibonacciEnumerable.cs
+++ FibonacciIterator/FibonacciEnumerable.cs
@@ -1,14 +1,39 @@
-﻿namespace FibonacciIterator
+﻿using System.Collections;
+
+namespace FibonacciIterator
 {
     /// <summary>
     /// Represents an enumerable object to iterate over the Fibonacci sequence numbers.
     /// </summary>
-    public sealed class FibonacciEnumerable
+    public sealed class FibonacciEnumerable : IEnumerable<int>
     {
-        // TODO Implement an enumerable to iterate over the Fibonacci sequence numbers.
+        private readonly int count;
+        private readonly int skipCount;
 
         public FibonacciEnumerable(int count = int.MaxValue, int skipCount = 0)
         {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            if (skipCount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(skipCount));
+            }
+
+            this.count = count;
+            this.skipCount = skipCount;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new FibonacciEnumerator(this.count, this.skipCount);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 }
--- FibonacciIterator/FibonacciEnumerator.cs
+++ FibonacciIterator/FibonacciEnumerator.cs
@@ -1,16 +1,100 @@
-﻿namespace FibonacciIterator
+﻿using System.Collections;
+
+namespace FibonacciIterator
 {
     /// <summary>
     /// Represents an enumerator object to iterate over the Fibonacci sequence numbers.
     /// </summary>
-    public sealed class FibonacciEnumerator
+    public sealed class FibonacciEnumerator : IEnumerator<int>
     {
-        // TODO Implement an enumerator to iterate over the Fibonacci sequence numbers.
+        private readonly int count;
+        private readonly int skipCount;
+        private int current;
+        private int previous;
+        private int index;
 
-        public void Dispose()
+        public FibonacciEnumerator(int count, int skipCount)
         {
-            // The method is empty, because there are no additional resources to dispose.
-            // See "Notes to Implementers" - https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ienumerator-1?view=net-6.0#notes-to-implementers
+            this.count = count;
+            this.skipCount = skipCount;
+            this.current = 0;
+            this.previous = 1;
+            this.index = 0;
+        }
+
+        public int Current
+        {
+            get
+            {
+                if (this.index == 0)
+                {
+                    throw new InvalidOperationException("Enumeration has not started. Call MoveNext().");
+                }
+
+                if (this.index > this.count)
+                {
+                    throw new InvalidOperationException("Enumeration has already ended.");
+                }
+
+                return this.current;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return this.Current; }
+        }
+
+        public bool MoveNext()
+        {
+            if (this.index >= this.count)
+            {
+                return false;
+            }
+
+            if (this.index < this.skipCount)
+            {
+                this.current += this.previous;
+                this.previous = this.current - this.previous;
+                this.index++;
+                return this.MoveNext();
+            }
+
+            if (this.index == this.skipCount)
+            {
+                this.index++;
+                return true;
+            }
+
+            if (this.current > int.MaxValue - this.previous)
+            {
+#pragma warning disable CS1717
+#pragma warning disable SA1101
+                this.current = current;
+#pragma warning restore SA1101
+#pragma warning restore CS1717
+            }
+            else
+            {
+                this.current += this.previous;
+            }
+
+            this.previous = this.current - this.previous;
+            this.index++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            this.current = 0;
+            this.previous = 1;
+            this.index = 0;
+        }
+
+#pragma warning disable S1186
+        public void Dispose()
+#pragma warning restore S1186
+        {
         }
     }
 }
